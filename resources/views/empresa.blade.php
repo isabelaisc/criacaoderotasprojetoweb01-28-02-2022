bf354d6ebd44e6662b9533372efa5a16bc2ab3b0
isabelaisc <isabelaisc2005@gmail.com>
2023-02-28 10:41:25 -0300

    first commit

---
+++ resources/views/empresa.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+</head>
+    <body>
+        <h1>Página Empresa!!!</h1>
+</body>
+</html>
---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+</head>
+    <body>
+        <h1>Página Produtos!!!</h1>
+</body>
+</html>
